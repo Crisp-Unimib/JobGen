778ff7509373354c07aa31ebefcf2ef37efb9ffb
Andrea Seveso <andrea.seveso@unimib.it>
2024-06-03 22:33:50 +0200

    utils

--- utils.py
+++ utils.py
@@ -24,9 +24,9 @@ def print_colored(text, color):
         print(text)
 
 
-def sample_skills(code_5d, df_occupations_and_skills, df_skills_count_per_occupation, global_skill_frequency):
+def sample_skills(code_4d, df_occupations_and_skills, df_skills_count_per_occupation, global_skill_frequency):
 
-    code_3d = int(code_5d[:-3])  # 4 digit code
+    code_3d = int(code_4d[:-3])  # 4 digit code
     # Get the occupation-specific dataframe at 3 digit
     occupation_df = df_skills_count_per_occupation[
         df_skills_count_per_occupation['code_3d'] == str(code_3d)]
@@ -34,20 +34,22 @@ def sample_skills(code_5d, df_occupations_and_skills, df_skills_count_per_occupa
     # Determine the average number of skills associated with this occupation
     # Divide by 2 to avoid too many skills
     try:
-        lambda_poisson = int(occupation_df['skill_to_job_ratio_3d'].iloc[0] / 2) ## il numero di skill da pescare deve essere al 4 digit (la somma dei 5)
+        # il numero di skill da pescare deve essere al 4 digit (la somma dei 5)
+        lambda_poisson = int(
+            occupation_df['skill_to_job_ratio_3d'].iloc[0] / 2)
     except:
         lambda_poisson = 10
         print("No skill found for", code_3d, "using default value of 10")
     n_skills_to_pick = np.random.poisson(lambda_poisson)
 
-
     # Ensure at least 1 skill is picked
-    n_skills_to_pick = max(n_skills_to_pick, 1) 
+    n_skills_to_pick = max(n_skills_to_pick, 1)
 
     # Obtain a list of unique skills for the occupation ## modified to sample from all the skills from the upper digit in ESCO ## QUA DEVO FARLA AL TERZO, se filtra MERDA A CASO RIPORTARE AL 4
     skill_list_for_this_3d = df_occupations_and_skills.copy()
-    skill_list_for_this_3d['code_3d'] = skill_list_for_this_3d['code_5d'].str[:-3]
-    skill_list_for_this_3d = skill_list_for_this_3d[skill_list_for_this_3d['code_3d'] == str(code_3d)]
+    skill_list_for_this_3d['code_3d'] = skill_list_for_this_3d['code_4d'].str[:-3]
+    skill_list_for_this_3d = skill_list_for_this_3d[skill_list_for_this_3d['code_3d'] == str(
+        code_3d)]
     unique_skills_list_per_occupation = skill_list_for_this_3d.preferredLabel_skill.unique()
 
     # Filter the global skills frequency for relevant occupation skills
@@ -56,31 +58,40 @@ def sample_skills(code_5d, df_occupations_and_skills, df_skills_count_per_occupa
 
     if not global_skill_frequency_filtered.empty:
 
-        # compute the distance between 3 digit code and code_5d
-        # skill_list_for_this_3d['distance'] = skill_list_for_this_3d['code_5d'].apply(lambda x: abs(int(x.replace('.','')) - int(code_3d)*100))
-        skill_list_for_this_3d['distance'] = skill_list_for_this_3d['code_5d'].str[3:].str.replace('.','') # in teoria basta fare cosi
+        # compute the distance between 3 digit code and code_4d
+        # skill_list_for_this_3d['distance'] = skill_list_for_this_3d['code_4d'].apply(lambda x: abs(int(x.replace('.','')) - int(code_3d)*100))
+        skill_list_for_this_3d['distance'] = skill_list_for_this_3d['code_4d'].str[3:].str.replace(
+            '.', '')  # in teoria basta fare cosi
 
         # merge skill_list_for_this_3d with global_skill_frequency_filtered in preferedLabel_skill and escoskill_level_3
-        global_skill_frequency_filtered = global_skill_frequency_filtered.merge(skill_list_for_this_3d[['preferredLabel_skill','distance']], left_on='escoskill_level_3', right_on='preferredLabel_skill', how='inner')
+        global_skill_frequency_filtered = global_skill_frequency_filtered.merge(skill_list_for_this_3d[[
+                                                                                'preferredLabel_skill', 'distance']], left_on='escoskill_level_3', right_on='preferredLabel_skill', how='inner')
         # drop duplicates in escoskill_level_3
-        global_skill_frequency_filtered.drop_duplicates(subset='escoskill_level_3', inplace=True)
-        
+        global_skill_frequency_filtered.drop_duplicates(
+            subset='escoskill_level_3', inplace=True)
+
         # Adjust probabilities so they sum to 1 ## NON E' GLOBAL MA DEL QUARTO DIGIT FILTRATO AL TERZO
         prob_sum = global_skill_frequency_filtered['cnt'].sum()
-        global_skill_frequency_filtered['probability_rescaled'] = global_skill_frequency_filtered['cnt'] / prob_sum ## NON E' GLOBAL MA DEL QUARTO DIGIT FILTRATO AL TERZO + PESATURA IN BASE AL DIGIT
+        # NON E' GLOBAL MA DEL QUARTO DIGIT FILTRATO AL TERZO + PESATURA IN BASE AL DIGIT
+        global_skill_frequency_filtered['probability_rescaled'] = global_skill_frequency_filtered['cnt'] / prob_sum
 
         # normalize inverse of distance
-        global_skill_frequency_filtered['distance'] = 1 / (global_skill_frequency_filtered['distance'].replace(0, np.inf).astype('float'))
+        global_skill_frequency_filtered['distance'] = 1 / (
+            global_skill_frequency_filtered['distance'].replace(0, np.inf).astype('float'))
         total_sum = global_skill_frequency_filtered['distance'].sum()
         global_skill_frequency_filtered['distance'] = global_skill_frequency_filtered['distance'] / total_sum
-    
+
         a = 0.9
-        global_skill_frequency_filtered['final_importance'] = global_skill_frequency_filtered['probability_rescaled'] * (1 - a) + a * (1 / global_skill_frequency_filtered['distance'].astype('float'))
+        global_skill_frequency_filtered['final_importance'] = global_skill_frequency_filtered['probability_rescaled'] * (
+            1 - a) + a * (1 / global_skill_frequency_filtered['distance'].astype('float'))
 
         # Ensure not to sample more skills than available
-        n_skills_to_pick = min(n_skills_to_pick, len(global_skill_frequency_filtered))
-        print("Skills available for sampling:", len(global_skill_frequency_filtered)) 
-        print(f"λ/2 (Poisson mean): {lambda_poisson}, value drawn: {n_skills_to_pick}") 
+        n_skills_to_pick = min(n_skills_to_pick, len(
+            global_skill_frequency_filtered))
+        print("Skills available for sampling:",
+              len(global_skill_frequency_filtered))
+        print(
+            f"λ/2 (Poisson mean): {lambda_poisson}, value drawn: {n_skills_to_pick}")
 
         # Sample skills based on their weighted probabilities
         selected_skills = global_skill_frequency_filtered.sample(
@@ -90,31 +101,34 @@ def sample_skills(code_5d, df_occupations_and_skills, df_skills_count_per_occupa
         selected_skills = occupation_df.sample(
             n=n_skills_to_pick, replace=False)
 
-    return selected_skills['escoskill_level_3'] # if not global_skill_frequency_filtered.empty else selected_skills['preferredLabel_skill']
+    # if not global_skill_frequency_filtered.empty else selected_skills['preferredLabel_skill']
+    return selected_skills['escoskill_level_3']
 
 
 def rescale_counts(df):
-    
+
     df['log_cnt'] = np.log(df['cnt'])
 
     # Rescaling function
     def scale_log_cnt(log_cnt, target_min, target_max, desired_mean):
 
-        # Min-Max 
+        # Min-Max
         min_log_cnt = df['log_cnt'].min()
         max_log_cnt = df['log_cnt'].max()
         # X - Xmin / Xmax - Xmin --> [0, 1]
         scaled_cnt = (log_cnt - min_log_cnt) / (max_log_cnt - min_log_cnt)
 
         # [0,1] --> [target_min, target_max]
-        range_scaled = scaled_cnt * (target_max - target_min) + target_min  # [0,1] * 28 + 2 --> [2, 30]
+        # [0,1] * 28 + 2 --> [2, 30]
+        range_scaled = scaled_cnt * (target_max - target_min) + target_min
 
         current_mean = range_scaled.mean()
         # [2, 30] --> [2, 30] + (10 - current_mean) --> [2, 30] + (10 - current_mean) --> [2, 30] con 10 di media
         corrected_scaled_cnt = range_scaled + (desired_mean - current_mean)
-        
+
         # Ensure the corrected values remain within the target range
-        corrected_scaled_cnt = np.clip(corrected_scaled_cnt, target_min, target_max)
+        corrected_scaled_cnt = np.clip(
+            corrected_scaled_cnt, target_min, target_max)
 
         return corrected_scaled_cnt
 
@@ -124,26 +138,29 @@ def rescale_counts(df):
     # Return the DataFrame with the new column 'rescaled_cnt'
     return df
 
+
 def get_rescaled_count(code_4d_value, rescaled_df):
     matching_row = rescaled_df.loc[rescaled_df['code_4d'] == code_4d_value]
     if not matching_row.empty:
         return int(matching_row['rescaled_cnt'].iloc[0])
     else:
         return 10  # Default value
-    
+
+
 def pick_seed_offline(code, oja_example_df):
 
     oja_example_df = oja_example_df[oja_example_df['found_code'] == code]
-    oja_example_df['job_ad'] = oja_example_df['title'] + '. ' + oja_example_df['description']
+    oja_example_df['job_ad'] = oja_example_df['title'] + \
+        '. ' + oja_example_df['description']
     oja_example_list = oja_example_df['job_ad'].tolist()
     return oja_example_list if len(oja_example_list) > 0 else ["No jobs found"]
 
 
 if __name__ == "__main__":
 
-    df_counts_5d = pd.read_csv("E:/uni/thesis/data/counts_5d.csv")
-    rescaled_df = rescale_counts(df_counts_5d)
-    for code_5d_value in df_counts_5d.code_5d.sample(5):
+    df_counts_4d = pd.read_csv("E:/uni/thesis/data/counts_4d.csv")
+    rescaled_df = rescale_counts(df_counts_4d)
+    for code_4d_value in df_counts_4d.code_4d.sample(5):
         # 1324.3
-        print("Rescaled count for", code_5d_value, ":", get_rescaled_count(code_5d_value))
-       
+        print("Rescaled count for", code_4d_value,
+              ":", get_rescaled_count(code_4d_value))
